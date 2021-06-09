d69b556fc148b8cefafcab78e0d25a42478aac2f
paulzay <paulotieno2@gmail.com>
2021-06-09 12:56:44 +0300

    finish destination carousel feature

--- lib/widgets/destination_carousel.dart
+++ lib/widgets/destination_carousel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:travelui/models/destination_model.dart';
 
 class DestinationCarousel extends StatelessWidget {
@@ -31,40 +32,42 @@ class DestinationCarousel extends StatelessWidget {
         ),
         Container(
           height: 300.0,
-          color: Colors.red,
           child: ListView.builder(
               scrollDirection: Axis.horizontal,
               itemCount: destinations.length,
               itemBuilder: (BuildContext context, int index) {
                 Destination destination = destinations[index];
                 return Container(
-                  margin: EdgeInsets.all(20.0),
+                  margin: EdgeInsets.all(10.0),
                   width: 210.0,
-                  alignment: Alignment.topCenter,
                   child: Stack(
+                    alignment: Alignment.topCenter,
                     children: [
-                      Container(
-                        height: 120.0,
-                        width: 200.0,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10.0)),
-                        child: Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                  '${destination.activities.length} activities',
-                                  style: TextStyle(
-                                    fontSize: 22.0,
-                                    fontWeight: FontWeight.w600,
-                                    letterSpacing: 1.2,
-                                  )),
-                              Text(destination.description,
-                                  style: TextStyle(color: Colors.grey)),
-                            ],
+                      Positioned(
+                        bottom: 15.0,
+                        child: Container(
+                          height: 120.0,
+                          width: 200.0,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0)),
+                          child: Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                    '${destination.activities.length} activities',
+                                    style: TextStyle(
+                                      fontSize: 22.0,
+                                      fontWeight: FontWeight.w600,
+                                      letterSpacing: 1.2,
+                                    )),
+                                Text(destination.description,
+                                    style: TextStyle(color: Colors.grey)),
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -80,11 +83,46 @@ class DestinationCarousel extends StatelessWidget {
                             ]),
                         child: Stack(
                           children: [
-                            Image(
-                              image: AssetImage(destination.imageUrl),
-                              height: 180.0,
-                              width: 180.0,
-                              fit: BoxFit.cover,
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Image(
+                                image: AssetImage(destination.imageUrl),
+                                height: 180.0,
+                                width: 180.0,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Positioned(
+                              left: 10.0,
+                              bottom: 10.0,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(destination.city,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 24.0,
+                                        fontWeight: FontWeight.w600,
+                                        letterSpacing: 1.2,
+                                      )),
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        FontAwesomeIcons.locationArrow,
+                                        size: 10.0,
+                                        color: Colors.white,
+                                      ),
+                                      SizedBox(
+                                        width: 5.0,
+                                      ),
+                                      Text(
+                                        destination.country,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
                             )
                           ],
                         ),
