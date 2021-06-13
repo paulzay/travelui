2948bfb65165ce887b93de686c9fd50ceef3a35f
paulzay <paulotieno2@gmail.com>
2021-06-13 22:54:05 +0300

    create hotel model class and a list of hotels

---
+++ lib/models/hotel_model.dart
@@ -0,0 +1,36 @@
+class Hotel {
+  String imageUrl;
+  String name;
+  String address;
+  int price;
+
+  Hotel({
+    this.imageUrl,
+    this.name,
+    this.address,
+    this.price,
+  });
+}
+
+final List<Hotel> hotels = [
+  Hotel(
+      imageUrl: 'assets/images/nguyen.jpg',
+      name: 'Kempinski',
+      address: '666 Park Avenue',
+      price: 500),
+  Hotel(
+      imageUrl: 'assets/images/nguyen.jpg',
+      name: 'Kempinski',
+      address: '666 Park Avenue',
+      price: 500),
+  Hotel(
+      imageUrl: 'assets/images/nguyen.jpg',
+      name: 'Kempinski',
+      address: '666 Park Avenue',
+      price: 500),
+  Hotel(
+      imageUrl: 'assets/images/nguyen.jpg',
+      name: 'Kempinski',
+      address: '666 Park Avenue',
+      price: 500),
+];
